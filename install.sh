8c08062c87b343ca706078d27d29e146f39aab5a
git user on sipw <root@sipw>
2012-04-09 19:35:30 +0200

    Minimal compatibility for RH/CentOS users in installer & service script

--- install.sh
+++ install.sh
@@ -4,6 +4,16 @@ set -e
 KELPIE_VERSION=0.1
 KELPIE_CONF=/usr/lib/kelpie-$KELPIE_VERSION/conf/server.properties
 
+# Identify Linux Flavour
+if [ -f /etc/debian_version ] ; then
+    PGROUP="nobody:nogroup"
+elif [ -f /etc/redhat-release ] ; then
+    PGROUP="nobody:nobody"
+else
+    PGROUP="nobody:nogroup"
+fi
+
+
 if [ -f $KELPIE_CONF ]
 then
     cp $KELPIE_CONF $KELPIE_CONF.bak
@@ -12,22 +22,22 @@ fi
 mvn -fpom.xml clean assembly::assembly
 
 INSTALL_ROOT=$(pwd)/target
-echo 'installing Kelpie Server ...' 
+echo 'Installing Kelpie Server ...' 
 cd /usr/lib && tar -xvzf $INSTALL_ROOT/kelpie-$KELPIE_VERSION.tar.gz 
 mkdir -p /var/run/kelpie/ 
 touch /var/run/kelpie/kelpied.pid 
-chown nobody:root /var/run/kelpie/ 
-chown nobody:nogroup /var/run/kelpie/kelpied.pid 
+chown $PGROUP /var/run/kelpie/ 
+chown $PGROUP /var/run/kelpie/kelpied.pid 
 mkdir -p /var/spool/kelpie/ 
 chmod 777 /var/spool/kelpie/ 
 mkdir -p /var/spool/kelpie/subscriptions/ 
 chmod 777 /var/spool/kelpie/subscriptions/ 
 mkdir -p /var/spool/kelpie/watchers/ 
 chmod 777 /var/spool/kelpie/watchers/ 
-chown -R nobody:nogroup /var/spool/kelpie
+chown -R $PGROUP /var/spool/kelpie
 mkdir -p /var/log/kelpie/ 
 chmod 777 /var/log/kelpie/ 
-chown -R nobody:nogroup /usr/lib/kelpie-$KELPIE_VERSION/ 
+chown -R $PGROUP /usr/lib/kelpie-$KELPIE_VERSION/ 
 ln -sf /usr/lib/kelpie-$KELPIE_VERSION/kelpied /etc/init.d/kelpied 
 
 if [ -f $KELPIE_CONF.bak ]
